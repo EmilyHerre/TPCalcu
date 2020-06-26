f8e87e922337492caaf6e0c72df36b135d97db90
Alana06 <51218720+Alana06@users.noreply.github.com>
2020-06-26 17:41:52 -0600

    VersionDocu

--- src/Operators/BracketsOperators.java
+++ src/Operators/BracketsOperators.java
@@ -6,24 +6,23 @@
 package Operators;
 
 /**
+ * This class has the parenthesis operators that are necessary to complete
+ * and understand the formula that the user enters. 
+*/
+
+/**
  *
- * @author Emily
+ * @author Emily Herrera, Alana Atencio, Sofia Guido.
  */
 public enum BracketsOperators {
     
-    /**
-     *
-     */
+    
     BRACKETSQUARE("[", "]", 4),
 
-    /**
-     *
-     */
+    
     BRACKETCIRCLE("(", ")", 4),
 
-    /**
-     *
-     */
+   
     BRACKETKEYS("{", "}", 4);
     
     private String bracketRight, bracketLeaft;
@@ -36,15 +35,15 @@ public enum BracketsOperators {
     }
 
     /**
-     *
-     * @return
+     *This method returns the parentheses on the right side
+     * @return right bracket
      */
     public String getBracketRight() {
         return bracketRight;
     }
 
     /**
-     *
+     * This method places a parenthesis on the right side
      * @param bracketRight
      */
     public void setBracketRight(String bracketRight) {
@@ -52,15 +51,15 @@ public enum BracketsOperators {
     }
 
     /**
-     *
-     * @return
+     *This method returns the parentheses on the left side
+     * @return left bracket
      */
     public String getBracketLeaft() {
         return bracketLeaft;
     }
 
     /**
-     *
+     *This method places a parenthesis on the left side
      * @param bracketLeaft
      */
     public void setBracketLeaft(String bracketLeaft) {
@@ -68,15 +67,15 @@ public enum BracketsOperators {
     }
 
     /**
-     *
-     * @return
+     * This method returns the priority
+     * @return priority
      */
     public int getPriority() {
         return priority;
     }
 
     /**
-     *
+     * This method places the priority
      * @param priority
      */
     public void setPriority(int priority) {
